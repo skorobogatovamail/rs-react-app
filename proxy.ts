947e69b6502a12276d25ed6c5d438146428cad49
skorobogatovamail <skorobogatovamail@gmail.com>
2026-06-22 18:48:55 +0300

    fix: fix build error

--- proxy.ts
+++ proxy.ts
@@ -10,10 +10,5 @@ export function proxy(request: NextRequest) {
 }
 
 export const config = {
-  // Match all pathnames except for
-  // - /api (API routes)
-  // - /_next (Next.js internals)
-  // - /_static (inside /public)
-  // - all root files inside /public (e.g. /favicon.ico)
   matcher: ['/((?!api|_next|_static|_vercel|[\\w-]+\\.\\w+).*)'],
 };
